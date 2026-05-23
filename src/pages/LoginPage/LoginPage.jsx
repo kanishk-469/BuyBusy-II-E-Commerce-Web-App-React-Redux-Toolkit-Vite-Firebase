2bf553c7ab020f173b10cbc40071f91c0bb5e43d
Kanishka Singh <kanishk.singh469@gmail.com>
2026-05-23 11:11:07 +0530

    Update on multiple pages
    Updated and created multiple components and performance optimized ,use useMemo, lazy load , use visualizer.

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { app, db } from "../../configDB/firebase";
 import { doc, getDoc } from "firebase/firestore";
+import Footer from "../../components/Footer/Footer";
 
 const auth1 = getAuth(app);
 
@@ -67,13 +68,17 @@ function LoginPage({ setIsLoggedIn, users, setUsers }) {
       const userDocSnap = await getDoc(userDocRef);
 
       if (userDocSnap.exists()) {
-        const userData = userDocSnap.data();
+        const loggedInUser = userDocSnap.data();
 
-        console.log("Full user data:", userData);
+        console.log("Full user data:", loggedInUser);
 
         // Set full user data in state
-        setUsers([userData]);
+        setUsers([loggedInUser]);
         setIsLoggedIn(true);
+
+        ///so that after refresh also logged In User won't gone
+        localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
+
         setFirebaseError("");
         Navigate("/cart");
       } else {
@@ -104,47 +109,50 @@ function LoginPage({ setIsLoggedIn, users, setUsers }) {
   }
 
   return (
-    <div className={style.container}>
-      <div className={style.formContainer}>
-        <h2 className={style.signupHeading}>Sign In</h2>
-        {firebaseError && <p className={style.error}>{firebaseError}</p>}
+    <>
+      <div className={style.container}>
+        <div className={style.formContainer}>
+          <h2 className={style.signupHeading}>Sign In</h2>
+          {firebaseError && <p className={style.error}>{firebaseError}</p>}
 
-        <form onSubmit={handleFormSubmit} className={style.signupForm}>
-          <div>
-            <input
-              type="email"
-              name="email"
-              className={style.formInput}
-              placeholder="Enter Email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            {errors.email && <p className={style.error}>{errors.email}</p>}
-          </div>
+          <form onSubmit={handleFormSubmit} className={style.signupForm}>
+            <div>
+              <input
+                type="email"
+                name="email"
+                className={style.formInput}
+                placeholder="Enter Email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              {errors.email && <p className={style.error}>{errors.email}</p>}
+            </div>
 
-          <div>
-            <input
-              type="password"
-              name="password"
-              className={style.formInput}
-              placeholder="Enter Password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-            {errors.password && (
-              <p className={style.error}>{errors.password}</p>
-            )}
-          </div>
+            <div>
+              <input
+                type="password"
+                name="password"
+                className={style.formInput}
+                placeholder="Enter Password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+              {errors.password && (
+                <p className={style.error}>{errors.password}</p>
+              )}
+            </div>
 
-          <button type="submit" className={style.signinBtn}>
-            Sign In
-          </button>
-          <Link to="/signup">
-            <p>Or Sign Up Instead</p>
-          </Link>
-        </form>
+            <button type="submit" className={style.signinBtn}>
+              Sign In
+            </button>
+            <Link to="/signup">
+              <p>Or Sign Up Instead</p>
+            </Link>
+          </form>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
